5faf5040325ae22465336817d258da184a87005f
mishthimahajan <mishthimahajan0@gmail.com>
2026-04-07 09:59:18 +0530

    Add adminController and fix import in AdminRoutes

--- backend/routes/AdminRoutes.js
+++ backend/routes/AdminRoutes.js
@@ -12,9 +12,4 @@ router.get("/users", getUsers);
 router.get("/gigs", getJobs);
 router.get("/payments", getPayments);
 
-// module.exports = router;
-module.exports = {
-    test: (req, res) => {
-        res.send("Admin controller working!");
-    }
-};
+module.exports = router;
